ade2e3f8ef31947a7a4aff4e292bcd1b4e73aa0d
Anibal Cucco <anibalcucco@gmail.com>
2023-02-17 14:09:10 -0300

    Remove unused code from app.js

--- src/javascripts/modules/app.js
+++ src/javascripts/modules/app.js
@@ -1,30 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { ThemeProvider, DEFAULT_THEME } from '@zendeskgarden/react-theming'
-import { Grid, Row, Col } from '@zendeskgarden/react-grid'
+import { Grid } from '@zendeskgarden/react-grid'
 import I18n from '../../javascripts/lib/i18n'
 import { resizeContainer, escapeSpecialChars as escape } from '../../javascripts/lib/helpers'
 import OpenAI from './openai'
 
 const MAX_HEIGHT = 1000
-const API_ENDPOINTS = {
-  ticketForms: '/api/v2/ticket_forms.json'
-}
-
-const API_KEY = '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';
 
 class App {
   constructor (client, _appData) {
     this._client = client
-
-    // this.initializePromise is only used in testing
-    // indicate app initilization(including all async operations) is complete
     this.initializePromise = this.init()
   }
 
-  /**
-   * Initialize module, render main template
-   */
   async init () {
     const currentUser = (await this._client.get('currentUser')).currentUser
 
@@ -41,14 +30,6 @@ class App {
       appContainer
     )
     return resizeContainer(this._client, MAX_HEIGHT)
-  }
-
-  /**
-   * Handle error
-   * @param {Object} error error object
-   */
-  _handleError (error) {
-    console.log('An error is handled here: ', error.message)
   }
 }
 
